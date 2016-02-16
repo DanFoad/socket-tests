79c8d1a35b7541ed80eb13ac80886559646a5ccf
Dan Foad <danielfoad@gmail.com>
2016-02-16 20:04:06 +0000

    Client written

--- DateServer/Client.java
+++ DateServer/Client.java
@@ -6,7 +6,14 @@ import java.net.Socket;
 import javax.swing.JOptionPane;
 
 public class Client {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        String serverAddress = JOptionPane.showInputDialog(
+            "Enter IP Address of a machine that is \r\n" +
+            "running the date service on port 9090:");
+        Socket s = new Socket(serverAddress, 9090);
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String answer = input.readLine();
+        JOptionPane.showMessageDialog(null, answer);
+        System.exit(0);
     }
 }
