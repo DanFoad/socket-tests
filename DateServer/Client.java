ad388b7befddd133f50f596f796b9440e723c594
Dan Foad <danielfoad@gmail.com>
2016-02-16 18:57:45 +0000

    Date Server initial setup

---
+++ DateServer/Client.java
@@ -0,0 +1,14 @@
+package co.uk.danfoad.sockettests;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ DateServer/Server.java
@@ -0,0 +1,13 @@
+package co.uk.danfoad.sockettests;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
